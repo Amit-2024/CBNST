c7db8dccc39f4b1f9e90c70c9e42c13487aedf69
Amit Semwal <100823495+Amit-2024@users.noreply.github.com>
2022-10-09 13:07:27 +0530

    Regula falsi Method

---
+++ Regula Falsi.cpp
@@ -0,0 +1,28 @@
+#include<iostream>          //a=2,b=3
+using namespace std;
+
+float fx(float x)
+{
+    return x*x*x-2*x-5;
+}
+
+int main()
+{
+    float a,b;
+    cin>>a>>b;
+
+    float x=0;
+    while(1)
+    {
+        x = (a*fx(b)-b*fx(a))/(fx(b)-fx(a));
+        cout<<x<<endl;
+        
+        if(abs(x-a)<=0.0001 || abs(x-a)<=0.0001)
+        break;
+
+        if(fx(x)*fx(a)<0)
+        b=x;
+        else
+        a=x;
+    }
+}
